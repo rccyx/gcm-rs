2fd8a977563b14836d4ed20a804346f6a3f7fc1a
rccyx <oss@rccyx.com>
2025-10-07 16:11:09 +0100

    fix error implementation

--- packages/rust/error.rs
+++ packages/rust/error.rs
@@ -1,3 +1,4 @@
+
 use std::fmt::{self, Display, Formatter};
 
 #[derive(Debug, PartialEq)]
@@ -11,14 +12,10 @@ impl Display for Error {
     fn fmt(&self, fmt: &mut Formatter) -> fmt::Result {
         match self {
             Error::InvalidNonceSize { expected_size } => {
-                write!(
-                    fmt,
-                    "invalid key size, expected {}",
-                    expected_size
-                )
+                write!(fmt, "invalid nonce size, expected {}", expected_size)
             }
             Error::InvalidKeySize => {
-                write!(fmt, "invalid key size",)
+                write!(fmt, "invalid key size")
             }
             Error::InvalidTag => {
                 write!(fmt, "invalid authentication tag")
@@ -28,3 +25,4 @@ impl Display for Error {
 }
 
 impl std::error::Error for Error {}
+
