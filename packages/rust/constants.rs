932afe9f1528c0938915f9d47881da03a8dd9622
rccyx <oss@rccyx.com>
2025-10-07 15:38:07 +0100

    update constants

--- packages/rust/constants.rs
+++ packages/rust/constants.rs
@@ -1,5 +1,5 @@
 pub const BLOCK_SIZE: usize = 16;
 pub const NONCE_SIZE: usize = BLOCK_SIZE - 4;
 pub const TAG_SIZE: usize = BLOCK_SIZE;
-pub const C_SIZE: usize = TAG_SIZE; // C for common size, ion mean none else
-pub const ZEROED_BLOCK: [u8; C_SIZE] = [0u8; C_SIZE];
+pub const ZEROED_BLOCK: [u8; BLOCK_SIZE] = [0u8; BLOCK_SIZE];
+
